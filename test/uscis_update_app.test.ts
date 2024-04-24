291a588625baaa6b8debc93da82183c342d99232
Smit Thakkar <smit@Smits-MacBook-Air.local>
2024-04-24 05:38:48 -0700

    Initial commit

---
+++ test/uscis_update_app.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as UscisUpdateApp from '../lib/uscis_update_app-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/uscis_update_app-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new UscisUpdateApp.UscisUpdateAppStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
